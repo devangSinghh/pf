b0b90063ac579dc4810bd05e08cf3d6ffab794f4
Devang <67219002+Luffy-webdev@users.noreply.github.com>
2021-03-15 23:22:24 +0530

    added blog features

--- client/src/blogEditor/blogFunctions.jsx
+++ client/src/blogEditor/blogFunctions.jsx
@@ -1,19 +1,142 @@
 import React, { Component } from 'react';
+import axios, { base } from '../axios-pf';
 
 class BlogFunctions extends Component {
 
-    appendInput = e => {
-        e.preventDefault();
-        const currentListLength = this.state.blogSections.length;
-        const newInput = `input-${ currentListLength }`;
-        this.setState(prevState => ({ blogSections: prevState.blogSections.concat([{name:newInput, val:""}]) }) );
+    handleClickOutside = e => {
+        {/*If clicked outside of sidebar, it will close*/ }
+        if (this.menuPaleteRef.current && !this.menuPaleteRef.current.contains(e.target)) {
+            {/*on clicking outside sidebar width will become 0*/ }
+            this.setState({ catchLineMenuState : false });
+        }
+    };
+    appendInput = async e => {
+        
+        const maxParaNumber = this.state.maxParaNumber
+        let payload;
+        payload = {name : `section-${maxParaNumber + 1}`, val : ""}
+
+        const {data : res} = await axios.post(base + `update-blog/thissection/${this.state.blog._id}`, payload)
+        console.log(res)
+        this.setState({ maxParaNumber: maxParaNumber + 1 })
+        const { data : totalParaInBlog } = await axios.post(base + `update-blog/total-blog-sections/${this.state.blog._id}/${this.state.maxParaNumber}`)
+        console.log(totalParaInBlog)
+    }
+
+    deleteThisSection = async(e, key, section) => {
+        const allSections = [...this.state.blogSections]
+
+        allSections.splice(key, 1)
+
+        this.setState({ blogSections : allSections })
+
+        const { data : res } = await axios.post(`/update-blog/delete-a-section/${this.state.blog._id}/${section}`)
     }
 
     handleBlogChange = (e, key) => {
         const val = e.target.value;
         const blogSections = [...this.state.blogSections]
+        // const k = blogSections[key] === undefined ? null : 
         blogSections[key].val = val;
         this.setState({ blogSections }) 
+    }
+
+    ChangeBannerImage = async({currentTarget:input}) => {
+        
+        const data = {...this.state.data};
+        data[input.name] = input.files[0]
+        
+        // data["bannerImageUrl"] = await URL.createObjectURL(input.files[0])
+        
+        this.setState({ data, dataIsValid:true });
+
+        const payload = new FormData() 
+        payload.append('blogfile', data.blogBanner)
+
+        const config = { headers: { 'content-type': 'multipart/form-data' } }
+
+        const {data : res} = await axios.put(base + `update-blog/${this.state.blog.name}`, payload, config)
+        console.log(res)
+    };
+
+    
+    saveBlog = async(e, key, sectionName) => {
+        const payload = {
+            content : this.state.blogSections[key].val
+        }
+
+        const { data : res } = await axios.put(base + `update-blog/section/${this.state.blog._id}/${sectionName}`, payload)
+        console.log(res)
+    }
+    
+    catchLine = () => {
+        this.setState({ showCatchLineInput : !this.state.showCatchLineInput })
+    }
+
+    addCatchLine = async() => {
+        const {data : res} = await axios.put(`update-blog/${this.state.blog.name}/${this.state.catchLine}`)
+        console.log(res)
+    }
+
+    openCatchLineMenu = () => {
+        this.setState({ catchLineMenuState : !this.state.catchLineMenuState })
+    }
+
+    handleChangeColor = async color => {
+        this.setState({ catchLineColor : color.hex });
+
+        const payload = await {color : color.hex}
+
+        const {data : res} = await axios.post(base + `update-blog/catchLine/${this.state.blog._id}`, payload)
+      };
+
+      changeFontFamilyOfCatchLine = async font => {
+        this.setState({ catchLineFontFamily : font })
+
+        const payload = await {fontFamily : font}
+
+        const { data : res } = await axios.post(base + `update-blog/catchLineFont/${this.state.blog._id}`, payload)
+      }
+
+      changeFontWeightOfCatchLine = async weight => {
+          this.setState({ catchLineFontWeight : weight })
+
+          const payload = await { fontWeight : weight }
+
+          const { data : res } = await axios.post(base + `update-blog/catchLineFontWeight/${this.state.blog._id}`, payload)
+      }
+
+      changeblogBackground = async background => {
+          this.setState({ changeblogBackgroundState : !this.state.changeblogBackgroundState })
+          
+      }
+
+      handleChangeBlogBackgroundColor = async color => {
+            this.setState({ changeblogBackground : color.hex })
+            const payload = { blogBackground : color.hex }
+            const { data : res } = await axios.post(base + `update-blog/blogBackground/${this.state.blog._id}`, payload)
+      }
+
+      changecontent = async background => {
+        this.setState({ changecontentState : !this.state.changecontentState })
+        
+    }
+
+    handleChangecontentColor = async color => {
+          this.setState({ contentColor : color.hex })
+          const payload = { contentColor : color.hex }
+          const { data : res } = await axios.post(base + `update-blog/content/${this.state.blog._id}`, payload)
+    }
+
+    handleBlogColorChange = e => {
+        const value = e.target.value;
+        this.setState({ blogCustomColor : value })
+    }
+
+    sendBlogCustomColorChange = async() => {
+        const payload = {blogBackground : '#'+this.state.blogCustomColor}
+        const { data : res } = await axios.post(base + `update-blog/blogBackground/${this.state.blog._id}`, payload)
+        
     }
 
     render() {
--- client/src/containers/eachBlog.jsx
+++ client/src/containers/eachBlog.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import axios, { base } from '../axios-pf';
 
-import colorfulBand from '../assets/colorful-band.svg';
+import BLogFunctions from '../blogEditor/blogFunctions';
 import { SwatchesPicker  } from 'react-color';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
-class EachBlog extends Component {
+class EachBlog extends BLogFunctions {
     menuPaleteRef = React.createRef()
     state = {
         blog : [],
@@ -92,141 +92,6 @@ class EachBlog extends Component {
             document.removeEventListener('click', this.handleClickOutside, true);
         };
     }
-    handleClickOutside = e => {
-        {/*If clicked outside of sidebar, it will close*/ }
-        if (this.menuPaleteRef.current && !this.menuPaleteRef.current.contains(e.target)) {
-            {/*on clicking outside sidebar width will become 0*/ }
-            this.setState({ catchLineMenuState : false });
-        }
-    };
-    appendInput = async e => {
-        
-        const maxParaNumber = this.state.maxParaNumber
-        let payload;
-        payload = {name : `section-${maxParaNumber + 1}`, val : ""}
-
-        const {data : res} = await axios.post(base + `update-blog/thissection/${this.state.blog._id}`, payload)
-        console.log(res)
-        this.setState({ maxParaNumber: maxParaNumber + 1 })
-        const { data : totalParaInBlog } = await axios.post(base + `update-blog/total-blog-sections/${this.state.blog._id}/${this.state.maxParaNumber}`)
-        console.log(totalParaInBlog)
-    }
-
-    deleteThisSection = async(e, key, section) => {
-        const allSections = [...this.state.blogSections]
-
-        allSections.splice(key, 1)
-
-        this.setState({ blogSections : allSections })
-
-        const { data : res } = await axios.post(`/update-blog/delete-a-section/${this.state.blog._id}/${section}`)
-    }
-
-    handleBlogChange = (e, key) => {
-        const val = e.target.value;
-        const blogSections = [...this.state.blogSections]
-        // const k = blogSections[key] === undefined ? null : 
-        blogSections[key].val = val;
-        this.setState({ blogSections }) 
-    }
-
-    ChangeBannerImage = async({currentTarget:input}) => {
-        
-        const data = {...this.state.data};
-        data[input.name] = input.files[0]
-        
-        // data["bannerImageUrl"] = await URL.createObjectURL(input.files[0])
-        
-        this.setState({ data, dataIsValid:true });
-
-        const payload = new FormData() 
-        payload.append('blogfile', data.blogBanner)
-
-        const config = { headers: { 'content-type': 'multipart/form-data' } }
-
-        const {data : res} = await axios.put(base + `update-blog/${this.state.blog.name}`, payload, config)
-        console.log(res)
-    };
-
-    
-    saveBlog = async(e, key, sectionName) => {
-        const payload = {
-            content : this.state.blogSections[key].val
-        }
-
-        const { data : res } = await axios.put(base + `update-blog/section/${this.state.blog._id}/${sectionName}`, payload)
-        console.log(res)
-    }
-    
-    catchLine = () => {
-        this.setState({ showCatchLineInput : !this.state.showCatchLineInput })
-    }
-
-    addCatchLine = async() => {
-        const {data : res} = await axios.put(`update-blog/${this.state.blog.name}/${this.state.catchLine}`)
-        console.log(res)
-    }
-
-    openCatchLineMenu = () => {
-        this.setState({ catchLineMenuState : !this.state.catchLineMenuState })
-    }
-
-    handleChangeColor = async color => {
-        this.setState({ catchLineColor : color.hex });
-
-        const payload = await {color : color.hex}
-
-        const {data : res} = await axios.post(base + `update-blog/catchLine/${this.state.blog._id}`, payload)
-      };
-
-      changeFontFamilyOfCatchLine = async font => {
-        this.setState({ catchLineFontFamily : font })
-
-        const payload = await {fontFamily : font}
-
-        const { data : res } = await axios.post(base + `update-blog/catchLineFont/${this.state.blog._id}`, payload)
-      }
-
-      changeFontWeightOfCatchLine = async weight => {
-          this.setState({ catchLineFontWeight : weight })
-
-          const payload = await { fontWeight : weight }
-
-          const { data : res } = await axios.post(base + `update-blog/catchLineFontWeight/${this.state.blog._id}`, payload)
-      }
-
-      changeblogBackground = async background => {
-          this.setState({ changeblogBackgroundState : !this.state.changeblogBackgroundState })
-          
-      }
-
-      handleChangeBlogBackgroundColor = async color => {
-            this.setState({ changeblogBackground : color.hex })
-            const payload = { blogBackground : color.hex }
-            const { data : res } = await axios.post(base + `update-blog/blogBackground/${this.state.blog._id}`, payload)
-      }
-
-      changecontent = async background => {
-        this.setState({ changecontentState : !this.state.changecontentState })
-        
-    }
-
-    handleChangecontentColor = async color => {
-          this.setState({ contentColor : color.hex })
-          const payload = { contentColor : color.hex }
-          const { data : res } = await axios.post(base + `update-blog/content/${this.state.blog._id}`, payload)
-    }
-
-    handleBlogColorChange = e => {
-        const value = e.target.value;
-        this.setState({ blogCustomColor : value })
-    }
-
-    sendBlogCustomColorChange = async() => {
-        const payload = {blogBackground : '#'+this.state.blogCustomColor}
-        const { data : res } = await axios.post(base + `update-blog/blogBackground/${this.state.blog._id}`, payload)
-        
-    }
 
     render() {
         const blog = this.state.blog === undefined ? null : this.state.blog
@@ -247,7 +112,7 @@ class EachBlog extends Component {
                 <button
                 onClick={this.addCatchLine}
                 className="catch-line-button">Save</button>}
-                {true && <div className="d-flex flex-column editor-menu">
+                {false && <div className="d-flex flex-column editor-menu">
                     <button className="mb-2" onClick={e => this.appendInput(e)}>add para</button>
                     <button className="mb-2" onClick={this.changeblogBackground}>Change blog background color</button>
                     <button className="mb-2" onClick={this.changecontent}>Change content color</button>
@@ -284,7 +149,7 @@ class EachBlog extends Component {
                 <div className="black-rect-shadow" />
 
                 <h5 style={{ color:this.state.catchLineColor, fontFamily:this.state.catchLineFontFamily, fontWeight:this.state.catchLineFontWeight }} className="blog-heading text-center">{blog.blogCatchLine} 
-                    <i onClick={this.openCatchLineMenu} className="catchLine-menu-bar fa fa-ellipsis-v" aria-hidden="true"></i>
+                    {/* <i onClick={this.openCatchLineMenu} className="catchLine-menu-bar fa fa-ellipsis-v" aria-hidden="true"></i> */}
                 </h5>
 
                 {this.state.catchLineMenuState && <div ref={this.menuPaleteRef} className="d-flex catchLine-menu-items">
@@ -323,7 +188,7 @@ class EachBlog extends Component {
 
                 {/* <img src={mask} className="mask" alt=""/> */}
                 {this.state.blog.blogSections === undefined ? null : this.state.blog.blogSections.map((m, key) => 
-                    <div className="container">
+                    <div className="container d-flex justify-content-center align-items-center">
                         <TextareaAutosize 
                         ref={(c) => (this.textarea = c)} 
                         style={{ color:this.state.contentColor }}
