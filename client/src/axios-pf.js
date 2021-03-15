518ac79abb6c6ebf1d5ff3868461a3a569a9b417
Devang <67219002+Luffy-webdev@users.noreply.github.com>
2021-03-15 23:29:10 +0530

    minor changes

--- client/src/axios-pf.js
+++ client/src/axios-pf.js
@@ -3,12 +3,12 @@ const instance = axios.create({
 
     // Base Domain Name
 
-    // baseURL: 'https://devang.design/api/'
-    baseURL: 'http://127.0.0.1:6161/'
+    baseURL: 'https://35.165.61.67/api/'
+    // baseURL: 'http://127.0.0.1:6161/'
 
 });
 
-// export let base = 'https://devang.design/';
-export let base = 'http://127.0.0.1:6161/';
+export let base = 'https://35.165.61.67/';
+// export let base = 'http://127.0.0.1:6161/';
 
 export default instance;
