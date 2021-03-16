17355fea940d1467a0c43738aea9e1fe58ed2ca4
Devang <67219002+Luffy-webdev@users.noreply.github.com>
2021-03-16 14:55:38 +0530

    aws push changes

--- client/src/axios-pf.js
+++ client/src/axios-pf.js
@@ -3,12 +3,12 @@ const instance = axios.create({
 
     // Base Domain Name
 
-    baseURL: 'https://65.0.108.90/' 
+    baseURL: 'http://65.0.108.90/' 
     // baseURL: 'http://127.0.0.1:6161/'
 
 });
 
-export let base = 'https://65.0.108.90/';
+export let base = 'http://65.0.108.90/';
 // export let base = 'http://127.0.0.1:6161/';
 
 export default instance;
