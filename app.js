41bfae4fd55137a330e7a2587b76fc5c20e9b5c8
Devang <67219002+Luffy-webdev@users.noreply.github.com>
2021-05-03 18:09:47 +0530

    added headers for no caching

--- app.js
+++ app.js
@@ -187,6 +187,9 @@ app.get('*', (req,res) =>{
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate") // HTTP 1.1.
+    res.setHeader("Pragma", "no-cache") // HTTP 1.0.
+    res.setHeader("Expires", "0") // Proxies.
     next()
 })
   
