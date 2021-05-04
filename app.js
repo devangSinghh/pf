5c09e0a007381883b7cb677e71b72ef9afe89aa6
Devang <67219002+Luffy-webdev@users.noreply.github.com>
2021-05-04 12:09:19 +0530

    #

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ app.use(function(req, res, next) {
     // res.setHeader("Expires", "0") // Proxies.
     next()
 })
-// app.use(nocache())
+app.use(nocache())
   
 app.listen(PORT, function() {
       console.log('server running on 6161')
